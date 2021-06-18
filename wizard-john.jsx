769797aafb1c383a44d3988b4a23ab520a873132
Dave <dave@citracommunications.com>
2021-06-18 08:27:47 -0700

    John's version

--- wizard-john.jsx
+++ wizard-john.jsx
@@ -18,8 +18,7 @@
 // 88888P Y88888 888   d88P   .d888888 888    888  888
 // 8888P   Y8888 888  d88P    888  888 888    Y88b 888
 // 888P     Y888 888 88888888 "Y888888 888     "Y88888
-//
-//
+
 // ______  _     _ __   _  ______ _______  _____  __   _ _______
 // |     \ |     | | \  | |  ____ |______ |     | | \  | |______
 // |_____/ |_____| |  \_| |_____| |______ |_____| |  \_| ______|
@@ -33,28 +32,38 @@
 // |_____/ __|__ |______ |_____ __|__ |  \_| |______ ______|
 //
 //
-//
-// Dave Blois, John(Sam) Fuchs
+// Dave Blois, John Sam Fuchs
 // June 16 2021
+
+// JSF 6/17/21 05:03PM PST
+// - Finished sheet now crops down to artwork bounds
+// - Added var "dimensions" for runtime access to artwork bounds
+// - Added details to file output name
+// - Created variables to use when implementing InfoTech data
+// - If/when we make a GUI for this, I'd love for most of the init variables to be changeable by the operator. Stuff like quantity, spacing, extra prints, etc should have some flexibility for changing workflows and media.
 
 var printFilePath = "/Users/grogtag/Desktop/WizardScripts/TestFiles-copy/TS_4x4 - Batch 12345-SoftTouchLamMatte Vinyl qty-10_PRINT.pdf"
 var infoFilePath = "/Users/grogtag/Desktop/WizardScripts/TestFiles-copy/TS_4x4 - Batch 12345-SoftTouchLamMatte Vinyl qty-10_INFO.pdf"
 var destination = "/Users/grogtag/Desktop/WizardScripts/TestFiles-copy/TS_4x4 - Batch 12345-SoftTouchLamMatte Vinyl qty-10_IMPO.pdf"
-var quantity = 50
-var space = 0.25
-var artWidth = 4
-var artHeight = 4
-var canvasWidth = 50
-var canvasHeight = 25
+var quantity = 100
+var space = 0.125
+var maxDocHeight = 48
+var maxDocWidth = 50
+var extraPrints = 4
+var orderNumber = '12345' //placeholder
+var SKU = '67891' //placeholder
+var dimensions = {}
+
 
 function saveAndClose(doc, dest) {
-  var saveName = new File(dest);
+  var finalDestination = (dest + orderNumber + "_" + SKU + "_" + dimensions.artWidth + "x" + dimensions.artHeight + "_qty" + quantity + ".pdf") // Is there a cleaner way to do this?
+  var saveName = new File(finalDestination);
   saveOpts = new PDFSaveOptions();
   saveOpts.compatibility = PDFCompatibility.ACROBAT5;
   saveOpts.generateThumbnails = true;
   saveOpts.preserveEditability = true;
   doc.saveAs(saveName, saveOpts);
-  doc.close();
+  doc.close()
 }
 
 // This damn thing insists that we use points instead of inches or centimeters. Well. I'm not gonna do that.
@@ -63,19 +72,21 @@ function points(inches) {
   return inches * 72;
 }
 
-function newFile(quantity, width, height, space, canvasWidth, canvasHeight, filePath, infoPath, dest) {
-  var printQuantity = quantity + 6;
+function newFile(quantity, extraPrints, width, height, space, canvasWidth, canvasHeight, filePath, infoPath, dest) {
+  var printQuantity = quantity + extraPrints;
   var filePath = File(filePath);
   var infoPath = File(infoPath);
-  var docWidth = points(canvasWidth);
-  var docHeight = points(canvasHeight);
+  var columns = Math.floor(points(canvasWidth) / (points(width) + points(space)));
+  var docHeight = Math.ceil(quantity / columns) * points(height + space);
+  var docWidth = (columns * points(width + space))
+
   var doc = app.documents.add(
     DocumentColorSpace.CMYK,
     docWidth,
     docHeight,
     1
   );
-  var columns = Math.floor( docWidth / ( points(width) + points(space) ) )
+
 
   var xPosition = 0;
   var yPosition = docHeight;
@@ -96,10 +107,10 @@ function newFile(quantity, width, height, space, canvasWidth, canvasHeight, file
     xPosition = xPosition + points(width) + points(space);
   }
   saveAndClose(doc, dest);
+
 }
 
 function InfoCut(width, height, positionX, positionY, infoPath) {
-  //FLAG - this is a repeat
   var infoPath = File(infoPath);
   open(infoPath);
   var width = points(width) - points(0.2)
@@ -126,8 +137,16 @@ function InfoCut(width, height, positionX, positionY, infoPath) {
   newRect.fillColor = NoColor;
   accDoc.close( SaveOptions.SAVECHANGES );
 }
-console.log("Hi, I'm Dave");
 
+// Initialize user-defined print file and get its data
+// Can later add to this to potentially grab order number, SKU, batch, etc from infoPath
+function InitFileVars(filePath, infoPath) {
+  var filePath = File(filePath);
+  open(filePath);
+  dimensions = {artWidth : (app.activeDocument.width / 72) , artHeight : (app.activeDocument.height / 72)} //keep 'dimensions' as inches for consistency
+  return dimensions
+}
 
-InfoCut(artWidth, artHeight, 0, artWidth, infoFilePath)
-newFile(quantity, artWidth, artHeight, space, canvasWidth, canvasHeight, printFilePath, infoFilePath, destination)
+InitFileVars(printFilePath, infoFilePath)
+InfoCut(dimensions.artWidth, dimensions.artHeight, 0, dimensions.artWidth, infoFilePath)
+newFile(quantity, extraPrints, dimensions.artWidth, dimensions.artHeight, space, maxDocWidth, maxDocHeight, printFilePath, infoFilePath, destination)
