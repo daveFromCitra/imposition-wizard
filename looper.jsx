a05e0a76cf265268683ac8c827a5a79e14c410c5
Dave <dave@citracommunications.com>
2021-06-28 14:35:11 -0700

    Trying to fix it

--- looper.jsx
+++ looper.jsx
@@ -61,35 +61,112 @@ function windowDisplay() {
   var submitButton = myButtonGroup.add("button", undefined, "Submit");
   submitButton.onClick = function() {
     FolderLooper(source, extraPrints, space);
-    // return w.close();
   }
 
   myButtonGroup.add("button", undefined, "Cancel");
   w.show();
 
+  return;
+}
 
+function saveAndClose(doc, dest) {
+  var saveName = new File(dest);
+  saveOpts = new PDFSaveOptions();
+  saveOpts.compatibility = PDFCompatibility.ACROBAT5;
+  saveOpts.generateThumbnails = true;
+  saveOpts.preserveEditability = true;
+  doc.saveAs(saveName, saveOpts);
+  doc.close();
+}
+
+function points(inches) {
+  return inches * 72;
+}
+
+function InfoCut(width, height, positionX, positionY, infoPath) {
+  //FLAG - this is a repeat
+  var infoPath = File(infoPath);
+  open(infoPath);
+  var width = points(width) - points(0.2)
+  var height = points(height) - points(0.2)
+  var positionX = points(positionX) + points(0.1)
+  var positionY = points(positionY) - points(0.1)
+
+  var accDoc = app.activeDocument;
+  var PerfCutSpot = accDoc.spots.add();
+  var spotCMYK = new CMYKColor();
+  spotCMYK.cyan = 100;
+  spotCMYK.magenta = 0;
+  spotCMYK.yellow = 100;
+  spotCMYK.black = 0;
+  PerfCutSpot.name = "PerfCutContour";
+  PerfCutSpot.colorType = ColorModel.SPOT;
+  PerfCutSpot.color = spotCMYK;
+  var PerfCutContour = new SpotColor();
+  PerfCutContour.spot = PerfCutSpot;
+  var newRect = accDoc.pathItems.rectangle(positionY, positionX, width, height)
+  newRect.stroked = true;
+  newRect.strokeWidth = 0.25;
+  newRect.strokeColor = PerfCutContour
+  newRect.fillColor = NoColor;
+  accDoc.close( SaveOptions.SAVECHANGES );
 }
 
 function fileNameParser(filename) {
+  var itemSpecs = {}
+  try {
+    // Regular Expression are the devil.
+    // These look for an obivous batch number in the file name string.
+    var BatchNumber = String(filename).match(/Batch%20\d{5}/)
+    var BatchNumber = String(BatchNumber).match(/(.{0,5})$/g)
 
-  var BatchNumber = String(filename).match(/Batch%20\d{5}/)
-  var BatchNumber = String(BatchNumber).match(/(.{0,5})$/g)
-  var width = String(filename).match(/TS_{1}/)
-  var width = String(width).match(/(.{0,1})$/g)
-  var height = String(filename).match(/TS_{3}/)
-  var height = String(height).match(/(.{0,1})$/g)
-  var quantity = String(filename).match((/?<=qty-)(.*)(?=_)/))
-  alert( BatchNumber + " needs " )
+    // These here look for an obvious width, then split it into just digits then into an Integer
+    var width = String(filename).match(/TS_(\d)/g)
+    var width = String(width).match(/(\d)/g)
+    var width = parseInt(width)
+    var height = String(filename).match(/TS_(\d)x(\d)/g)
+    var height = String(height).match(/x(\d)/g)
+    var height = String(height).match(/(\d)/g)
+    var height = parseInt(height)
+    var quantity = String(filename).match(/(qty-)(\d{1,5})/g)
+    var quantity = String(quantity).match(/(\d{1,5})/g)
+    var quantity = parseInt(quantity)
+
+    itemSpecs.Batch = BatchNumber;
+    itemSpecs.Width = width;
+    itemSpecs.Height = height;
+    itemSpecs.Quantity = quantity;
+
+    return itemSpecs;
+  } catch (e) {
+    alert(filename + " Shows invalid file name")
+  } finally {
+
+  }
+
 }
+
+
 
 function FolderLooper(srcFolder, destinationFolder, extraPrints, space) {
   var csvOrder = srcFolder.getFiles(/\.csv$/i)
   var allPrintPDFs = srcFolder.getFiles(/PRINT\.pdf$/i);
   var allInfoPDFs = srcFolder.getFiles(/INFO\.pdf$/i);
 
+  //This runs infoCut on all infoTech files
   for (var i = 0; i < allInfoPDFs.length; i++) {
-    fileNameParser(allInfoPDFs[i]);
+    var itemSpecs = fileNameParser(allInfoPDFs[i]);
+    InfoCut(itemSpecs.Width, itemSpecs.Height, 0, itemSpecs.Width, allInfoPDFs[i]);
   }
+
+  //This loop will open all files
+  // for (var i = 0; i < allPrintPDFs.length; i++) {
+  //   var itemSpecs = fileNameParser(allPrintPDFs[i]);
+  //
+  // }
+  return;
 }
+
+
 
 windowDisplay();
